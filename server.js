cfc8b96064945725243e456ea8efe3a4bcffbb75
Phyo Wai Aung <phyowaiaung@yomabank.com>
2019-12-11 10:25:18 +0630

    Final Code For Exercise

--- server.js
+++ server.js
@@ -5,13 +5,50 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 const http = require('http');
+const moment = require('moment-timezone');
+moment.tz.setDefault('UTC');
+const serialize = require('serialize-javascript');
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
+//app.use('/dist', express.static(path.join(__dirname, 'dist')));
+
+let renderer;
+
+if(process.env.NODE_ENV === 'production'){
+  let bundle = fs.readFileSync('./dist/node.bundle.js', 'utf8');
+  renderer = require('vue-server-renderer').createBundleRenderer(bundle);
+  app.use('/dist', express.static(path.join(__dirname, 'dist')));
+}
+
+let events = [
+  {description: 'Random event 1', date: moment('2017-02-6', 'YYYY-MM-DD')},
+  {description: 'Random event 2', date: moment('2017-02-15', 'YYYY-MM-DD')},
+  {description: 'Random event 3', date: moment('2017-03-15', 'YYYY-MM-DD')}
+];
 
 app.get('/', (req, res) => {
   let template = fs.readFileSync(path.resolve('./index.html'), 'utf-8');
-  res.send(template);
+  let contentMarker = '<!--App-->';
+  if(renderer){
+    renderer.renderToString({}, (err, html) => {
+      if(err){
+        console.log(err);
+      }else{
+        res.send(template.replace(contentMarker, `<script>var __INITIAL_STATE__ = ${ serialize(events) }</script>\n${html}`));
+      }
+    });
+  }else{
+    res.send('<p>Awaiting compilation..</p><script src="/reload/reload.js"></script>')
+  }
+});
 
+
+
+app.use(require('body-parser').json());
+
+app.post('/add_event', (req, res) => {
+  events.push(req.body);
+  res.sendStatus(200);
 });
 
 const server = http.createServer(app);
@@ -20,6 +57,13 @@ if (process.env.NODE_ENV === 'development') {
   const reload = require('reload');
   const reloadServer = reload(app);
   require('./webpack-dev-middleware').init(app);
+  require('./webpack-server-compiler').init(function(bundle){
+    let needsReload = (renderer === undefined);
+    renderer = require('vue-server-renderer').createBundleRenderer(bundle);
+    if(needsReload){
+      reloadServer.reload();
+    }
+  });
 }
 
 server.listen(process.env.PORT, function () {
